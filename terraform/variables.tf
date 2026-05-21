a5b375c8c4ca9d0c35e4970dc99b565c91bd90a6
Karingi <emmanuelkaringi@gmail.com>
2026-05-21 12:37:22 +0300

    Add default values for all Terraform variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -22,16 +22,19 @@ variable "vpc_cidr" {
 variable "public_subnet_cidrs" {
   description = "CIDR blocks for public subnets"
   type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnet_cidrs" {
   description = "CIDR blocks for private subnets"
   type        = list(string)
+  default     = ["10.0.10.0/24", "10.0.11.0/24"]
 }
 
 variable "availability_zones" {
   description = "List of availability zones"
   type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
 }
 
 # EC2
